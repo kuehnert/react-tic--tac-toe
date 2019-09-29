86da252caa83b0c5a8321ddeea3de669ec8478a0
Matthias Kuehnert <matthias.kuehnert@marienschule.com>
2019-09-29 17:03:25 +0200

    Updates

---
+++ precache-manifest.c552eb1cb2c6e12559b75b173abdeaa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff70dd5ab508fdfddbf16c83ec6acc22",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "73f5685c0360bcbb0815",
+    "url": "/react-tic-tac-toe/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e2116853105ae68e54e0",
+    "url": "/react-tic-tac-toe/static/css/main.086417f1.chunk.css"
+  },
+  {
+    "revision": "73f5685c0360bcbb0815",
+    "url": "/react-tic-tac-toe/static/js/2.0b68f48f.chunk.js"
+  },
+  {
+    "revision": "e2116853105ae68e54e0",
+    "url": "/react-tic-tac-toe/static/js/main.6b03c587.chunk.js"
+  },
+  {
+    "revision": "9d9be5a11996a713ad82",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.cf5e814e.js"
+  }
+]);
